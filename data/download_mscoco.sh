6ddcc5722ac3ae4d5a98fb3b451ce68641397d46
Pavlos Melissinos <pmelissi@iti.gr>
2017-04-08 03:03:11 +0300

    Add bash script to download mscoco

---
+++ data/download_mscoco.sh
@@ -0,0 +1,8 @@
+# train set
+wget -P mscoco http://msvocds.blob.core.windows.net/coco2014/train2014.zip
+
+# validation set
+wget -P mscoco http://msvocds.blob.core.windows.net/coco2014/val2014.zip
+
+# annotations
+wget -P mscoco http://msvocds.blob.core.windows.net/annotations-1-0-3/instances_train-val2014.zip
