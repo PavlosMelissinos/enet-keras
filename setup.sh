f03912a1935c396a6d769887f319e794e1790b3a
Pavlos Melissinos <pmelissi@iti.gr>
2017-10-10 19:33:29 +0300

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,4 @@
+cp config/solver.json.default config/solver.json
+mkdir pretrained
+wget -P pretrained https://www.dropbox.com/sh/dywzk3gyb12hpe5/AABoUwqQGWvClUu27Z1EWeu9a/model-best.net
+python src/models/from_torch.py
